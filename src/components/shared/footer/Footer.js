02efa91bdc58ce30a280937ce1808da29de26ed1
portafolio <edgar.cortes1812@gmail.com>
2024-01-21 16:11:45 -0600

    se alinio texto

--- src/components/shared/footer/Footer.js
+++ src/components/shared/footer/Footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
     <footer style={footerStyle} className="py-2">
       <div className='container'>
         <div className="row">
-          <div className="col-md-6 text-start mt-3">
+          <div className="col-md-6 text-md-start text-center mt-3">
             <p>Desarrollado por <img src="/img/logoBinasCode.jpg" width={25} alt="" /> <a href='https://binascode.com/'>https://binascode.com/</a></p>
           </div>
-          <div className="col-md-6 text-end mt-3">
+          <div className="col-md-6 text-md-end text-center mt-3">
             <p>© 2023 Fraternidad Sendero de Luz México. Todos los derechos reservados.</p>
           </div>
         </div>
